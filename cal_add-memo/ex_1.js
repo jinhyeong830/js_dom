184a4ee8459057e95997a013093b28c313bb749b
jinhyeong830 <shg02387@naver.com>
2022-07-19 20:10:37 +0900

    sc

--- cal_add-memo/ex_1.js
+++ cal_add-memo/ex_1.js
@@ -1,37 +1,37 @@
 const cal=document.querySelector("table");
 const date=document.querySelector("#date");
 const content=document.querySelector("#content");
-const btn=document.querySelector("button");
-
-
-// let pParent=e.target.parentNode;
-
+// const btn=document.querySelector("button"); html 상에서 onclick걸어줘서 변수선언 따로 필요 없음
+let pParent; //parent 미리 선언
 
 cal.addEventListener("click",function(e){
-    console.log(e.target.tagName);
     if(e.target.tagName==="P"){
         date.value=e.target.textContent;
+        pParent=e.target.parentNode;
+    }else {
+        // 테이블 TD가 클릭 되면 TD를 전역 변수에 저장하여 위치 전달 why?
+        //날짜 뿐만 아니라 날짜에 대한 칸을 눌러도 동작하도록
+        date.value = e.target.firstChild.textContent;
+        pParent = e.target;
     }
-    let pParent=e.target.parentNode;
-    console.log(pParent);
-    btn.addEventListener("click",writeSchedule);
-    function writeSchedule(){
-        const dv=document.createElement("div");
-        dv.textContent=content.value;
-        pParent.append(dv);
-    }
-   
-   
+});
+
+    //전역 함수 만들기
+function writeSchedule(){
+    let dv=document.createElement("div");
+    dv.textContent=content.value;
+    pParent.append(dv);
+    content.value=""; //클릭 누르면 txt창 초기화시키기
+    date.value="";
+}
+        
+         
     
-})
+    
+     
 
 
-// btn.addEventListener("click",function);
-// function writeSchedule(e){
-//     c
 
-// }
 
-//어디다가 붙이라고 e.target을 전역으로 선언해서 하셨다고 합니당
 
 
